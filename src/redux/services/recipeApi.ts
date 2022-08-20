e3adeafeab888f0f1017a86daeaed7fff8b447c9
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-20 22:20:06 +0530

    Initialized setup and configured redux store using redux toolkit

---
+++ src/redux/services/recipeApi.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const YOUR_APP_KEY = "f77677604f950263fc7eb35f4d66e111";
+const YOUR_APP_ID = "cbc6acf1";
+
+export const recipeApi = createApi({
+  reducerPath: "recipeApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.edamam.com/",
+  }),
+  endpoints: (builder) => ({
+    getRecipes: builder.mutation({
+      query: ({ query, health }) => {
+        return {
+          url: `search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&health=${health}`,
+        };
+      },
+    }),
+  }),
+});
+
+export const { useGetRecipesMutation } = recipeApi;
