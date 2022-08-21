ddb54f701d3a3a2915d5e705252e45a60de4a2d9
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-21 16:12:28 +0530

    Completed Recipe App with search and drop down functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App";
 import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
+
+import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
--- src/redux/services/recipeApi.ts
+++ src/redux/services/recipeApi.ts
@@ -5,14 +5,13 @@ const YOUR_APP_ID = "cbc6acf1";
 
 export const recipeApi = createApi({
   reducerPath: "recipeApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.edamam.com/",
-  }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.edamam.com/" }),
   endpoints: (builder) => ({
     getRecipes: builder.mutation({
       query: ({ query, health }) => {
         return {
           url: `search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&health=${health}`,
+          method: "GET",
         };
       },
     }),
